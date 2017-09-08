2b32a92b4d6f6e25d54a61e395351d95d47349d9
Deshun <1209278955@qq.com>
2017-09-09 01:04:45 +0800

    2017-9-9

---
+++ index.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>刘德顺-个人简历</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/navFixed.js"></script>
+    <script src="js/main.js"></script>
+</head>
+<body>
+<section id="firstPage">
+    <p>Hello World</p>
+    <div id="fstPage-down" class="wow infinite bounce" data-wow-delay="1s"><a href="#intro" hideFocus="true"><i
+                    class="fa fa-angle-double-down"></i></a></div>
+</section>
+<nav class="navbar navbar-default" id="nav" style="position: relative">
+    <div class="container-fluid container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand fl" href="#firstPage" id="logo" hideFocus="true">刘德顺</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#intro" id="nav-intro" hideFocus="true">个人简介</a></li>
+                <li><a href="#skill" id="nav-skill" hideFocus="true">专业技能</a></li>
+                <li><a href="#workEXP" id="nav-workEXP" hideFocus="true">工作经验</a></li>
+                <li><a href="#prjEXP" id="nav-prjEXP" hideFocus="true">项目经验</a></li>
+                <li><a href="#wantMore" id="nav-wantMore" hideFocus="true">联系方式</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+<section id="intro" class="container">
+    <div class="container">
+        <div class="row">
+            <div class="intro-logo col-md-12 col-sm-4">
+                <div class="intro col-md-2 wow bounceInDown"><a href="https://github.com/ideshun" target="_blank"><i class="fa fa-github"></i></a></div>
+                <div class="intro col-md-2 wow bounceInDown" data-wow-delay=".2s"><a href="https://www.linkedin.com/in/%E5%BE%B7%E9%A1%BA-%E5%88%98-10a487147/" target="_blank"><i class="fa fa-linkedin-square"></i></a></div>
+                <div class="intro col-sm-12 col-md-4 wow bounceInDown" data-wow-delay=".3s" id="intro-center"><a href="#"><i></i></a></div>
+                <div class="intro col-md-2 wow bounceInDown" data-wow-delay=".2s"><a href="https://plus.google.com/111583209340330633003" target="_blank"><i class="fa fa-google-plus-square"></i></a></div>
+                <div class="intro col-md-2 wow bounceInDown"><a href="mailto:admin@w3h5.com"><i class="fa fa-envelope-square"></i></a>
+                </div>
+                <div class="intro-logo-little col-sm-12 col-xs-6 col-sm-offset-0 col-xs-offset-3">
+                    <div class="col-sm-3 col-xs-3"><a href="https://github.com/ideshun" target="_blank"><i class="fa fa-github"></i></a></div>
+                    <div class="col-sm-3 col-xs-3"><a href="https://www.linkedin.com/in/%E5%BE%B7%E9%A1%BA-%E5%88%98-10a487147/" target="_blank"><i class="fa fa-linkedin-square"></i></a></div>
+                    <div class="col-sm-3 col-xs-3"><a href="https://plus.google.com/111583209340330633003" target="_blank"><i class="fa fa-google-plus-square"></i></a></div>
+                    <div class="col-sm-3 col-xs-3"><a href="mailto:admin@w3h5.com"><i class="fa fa-envelope-square"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="intro-text col-md-4 col-sm-7 col-xs-6 col-md-offset-0 col-sm-offset-1 col-xs-offset-3 wow bounceInLeft"data-wow-delay=".5s">
+                <ul>
+                    <li><span>姓名</span> | 刘德顺</li>
+                    <li><span>现居</span> | 上海-杨浦</li>
+                    <li><span>手机</span> | <a href="tel:17601230883">17601230883</a></li>
+                    <li><span>Gmail</span> | <a href="mailto:deshunge@gmail.com">deshunge
+                            <spa class="display-none">@gmail.com</spa>
+                        </a></li>
+                    <li><span>GitHub</span> | <a href="https://github.com/ideshun" target="_blank">
+                            <spa class="display-none">github.com/</spa>
+                            ideshun</a></li>
+                    <li><span>My Blog</span> | <a href="http://www.w3h5.com" target="_blank">
+                            <spa class="display-none">www.</spa>
+                            w3h5.com</a></li>
+                    <li><span>Linkedin</span> | <a
+                                href="https://www.linkedin.com/in/%E5%BE%B7%E9%A1%BA-%E5%88%98-10a487147/"
+                                target="_blank">德顺-刘</a></li>
+                </ul>
+            </div>
+            <div class="intro-text col-md-8 wow bounceInRight" data-wow-delay=".8s">
+                <p>
+                    我喜欢研究遇到的各种技术，不给自己设边界，有 Geek 精神。
+                </p>
+                <p>
+                    有时间概念，能在约定时间内保证完成任务。
+                </p>
+                <p>
+                    一年前端开发经验，熟悉 HTML、CSS、Javascript，能熟练开发符合 W3C 标准的页面，熟练使用 Bootstrap 等前端框架，了解AngularJS、Nodejs的基本操作。</p>
+                <p>
+                    有良好的编码习惯和代码等个。
+                </p>
+                <p>
+                    有 Linux 基础，对服务器端开发有一定了解，能够配合程序员完成相关工作。
+                    了解 PHP+MySQL；理解基本的算法分析与数据结构。
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="skill">
+    <div class="container">
+        <div class="title wow fadeIn" data-wow-delay="1s">
+            专业技能
+        </div>
+        <div class="row">
+            <div class="col-sm-3 col-xs-12 wow bounceInDown" data-wow-delay=".8s">
+                <ul>
+                    <li>语言</li>
+                    <li>HTML(5)</li>
+                    <li>CSS(3)</li>
+                    <li>Javascript</li>
+                    <li>PHP</li>
+                    <li>SQL</li>
+                </ul>
+            </div>
+            <div class="col-sm-3 col-xs-12 wow bounceInDown" data-wow-delay=".6s">
+                <ul>
+                    <li>思想</li>
+                    <li>W3C标准</li>
+                    <li>语义化标签</li>
+                    <li>表现与结构分离</li>
+                    <li>响应式布局</li>
+                    <li>精简代码</li>
+                    <li>追求极致</li>
+                </ul>
+            </div>
+            <div class="col-sm-3 col-xs-12 wow bounceInDown" data-wow-delay=".4s">
+                <ul>
+                    <li>框架/类库</li>
+                    <li>jQuery</li>
+                    <li>Bootstrap</li>
+                    <li>AngularJS</li>
+                    <li>Animate.css</li>
+                    <li>express</li>
+                    <li>Less</li>
+                    <li>MUI</li>
+                </ul>
+            </div>
+            <div class="col-sm-3 col-xs-12 wow bounceInDown" data-wow-delay=".2s">
+                <ul>
+                    <li>技术</li>
+                    <li>Git</li>
+                    <li>Linux</li>
+                    <li>Apache</li>
+                    <li>Photoshop</li>
+                    <li>Premiere</li>
+                    <li>After Effects</li>
+                    <li>Flash</li>
+                    <li>3DMax/Maya</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="workEXP">
+    <div class="container">
+        <div class="title wow bounceInUp">
+            工作经验
+        </div>
+        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="panel panel-primary wow bounceInUp" data-wow-delay=".2s">
+                <div class="panel-heading" role="tab" id="headingOne">
+                    <h4 class="panel-title">
+                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne"
+                           aria-expanded="true" aria-controls="collapseOne">
+                            2014.08 《黑白校园》华北区校园大使
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                    <div class="panel-body">
+                        受邀成为《黑白校园》华北区校园大使，负责华北区各大高校的宣传推广，纳新工作，成功面试并筛选出数十座高校的校园大使，保证9月份《黑白校园》的成功上线。
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-primary wow bounceInUp" data-wow-delay=".4s">
+                <div class="panel-heading" role="tab" id="headingTwo">
+                    <h4 class="panel-title">
+                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+                           href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            2014.09 新时空文化传媒（工作室）
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                    <div class="panel-body">
+                        组织团队，建立新时空文化传媒工作室，主要经营校内的文件打印、复印，平面设计、台历设计制作、名片设计制作等；<br>
+                        运营淘宝店，运用百度地图、百度糯米进行SEO免费推广接单；<br>
+                        同时带新，毕业后将工作室转售给下一级学弟。
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-primary wow bounceInUp" data-wow-delay=".5s">
+                <div class="panel-heading" role="tab" id="headingThree">
+                    <h4 class="panel-title">
+                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+                           href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            2016.07-2017.07 山东小爱文化传媒有限公司
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                    <div class="panel-body">
+                        公司网站前端页面制作、H5页面制作、网站后台内容更新；<br>
+                        公司旗下网站深圳市聚游信息科技有限公司网站制作，网页兼容性调试等；<br>
+                        参与公司外包项目的前端页面制作，涉及H5C3、JavaScript、前端框架、AngularJS、Node.js等。
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section id="prjEXP">
+    <div class="container">
+        <div class="title wow fadeIn">
+            项目经验
+        </div>
+        <div class="row wow fadeInUp" data-wow-delay=".2s">
+            <div class="col-sm-4 col-md-3 col-xs-6">
+                <div type="button" class="btn btn-default" data-toggle="tooltip" data-placement="bottom"
+                     title="深圳聚游科技有限公司">
+                    <img src="img/juyou.png" height="50" alt=""></div>
+            </div>
+            <div class="col-sm-4 col-md-3 col-xs-6">
+                <div type="button" class="btn btn-default" data-toggle="tooltip" data-placement="bottom"
+                     title="tap4fun移动端">
+                    <img src="img/tap4fun.png" height="50" alt=""></div>
+            </div>
+            <div class="col-sm-4 col-md-3 col-xs-6">
+                <div type="button" class="btn btn-default" data-toggle="tooltip" data-placement="bottom" title="德顺网络论坛">
+                    <a href="http://mz.52deshun.com" target="_blank"><img src="img/dswllt.png" height="50" alt=""></a>
+                </div>
+            </div>
+            <div class="ccol-sm-4 col-md-3 col-xs-6">
+                <div type="button" class="btn btn-default" data-toggle="tooltip" data-placement="bottom" title="德顺博客"><a
+                            href="http://www.dblog.cc" target="_blank"><img src="img/dblog.png" height="50" alt="">
+                </div>
+                </a>
+            </div>
+            <div class="col-sm-4 col-md-3 col-xs-6">
+                <div type="button" class="btn btn-default" data-toggle="tooltip" data-placement="bottom"
+                     title="w3h5-前端资源网">
+                    <a href="http://www.w3h5.com" target="_blank"><img src="img/w3h5.png" height="50" alt=""></a></div>
+            </div>
+            <div class="col-sm-4 col-md-3 col-xs-6">
+                <div type="button" class="btn btn-default" data-toggle="tooltip" data-placement="bottom" title="爱缩网址缩短">
+                    <a href="http://iiso.vip" target="_blank"><img src="img/iiso.png" height="50" alt=""></a></div>
+            </div>
+            <div class="col-sm-4 col-md-3 col-xs-6">
+                <div type="button" class="btn btn-default" data-toggle="tooltip" data-placement="bottom" title="紫罗兰冲施肥">
+                    <a href="http://www.ydlll.cn" target="_blank">紫罗兰冲施肥</a></div>
+            </div>
+            <div class="col-sm-4 col-md-3 col-xs-6">
+                <div type="button" class="btn btn-default" data-toggle="tooltip" data-placement="bottom"
+                     title="图片二维码生成器">
+                    <a href="http://www.w3h5.com/QRCode" target="_blank">图片二维码生成器</a></div>
+            </div>
+        </div>
+    </div>
+
+
+</section>
+
+<section id="wantMore">
+    <p>
+        联系方式：
+    </p>
+    <p>
+        <a href="tencent://Message/?Uin=1209278955"><i class="fa fa-qq"></i></a> <a id="weChat" href="javascript:;"><i
+                    class="fa fa-wechat"></i><img id="weChat-img" src="img/weChat.png" width="150px"
+                                                  style="position: relative;top:0;left:0;" alt=""></a>
+    </p>
+</section>
+
+<script>
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+        $('#weChat-img').hide();
+        $('#weChat').hover(function () {
+            $('.fa-wechat').hide();
+            $('#weChat-img').show();
+        }, function () {
+            $('.fa-wechat').show();
+            $('#weChat-img').hide();
+        });
+        new WOW().init();
+    })
+</script>
+</body>
+</html>
